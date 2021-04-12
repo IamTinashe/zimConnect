db815861c0210e12408b8c74a24d0149c123ffbd
IamTinashe <tinashetp@gmail.com>
2021-04-12 18:47:25 +0200

    creates_homepage

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="container-fluid bgColor-black Color-white">
-    Hello
+  <div class="conatiner-fluid">
+    <div class="container-fluid bgColor-black Color-white py-4">
+      <div class="row justify-content-md-center mx-lg-2">
+        <div class="col-md-5">
+          <span>PHONE: +75 8746 958</span>
+          <span class="ml-2">EMAIL: ZIMCONNECT@ZIMWORX.COM</span>
+        </div>
+        <div class="col-md-4 col-lg-5"></div>
+        <div class="col-md-3 col-lg-2 text-right">
+          <span class="mr-4">SIGNUP</span>
+          <span>SIGNIN</span>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bgColor-gray-10 py-4">
+      <div class="container">
+        <div class="row justify-content-md-center">
+          <div class="col-md-3">
+            <h1 CLASS="title">ZIMCONNECT</h1>
+          </div>
+          <div class="col-md-6"></div>
+          <div class="col-md-3">
+            <div class="button button-primary button-semi-pill contracted">
+              FOR EMPLOYERS
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,56 @@
 <template>
-  <section class="container">
-    Hello
-  </section>
+  <div class="conatiner-fluid">
+    <div class="container-fluid bgColor-gray-30 py-5">
+      <div class="container pb-5">
+        <div class="row py-5">
+          <div class="col-md-12 pt-5">
+            <p class="text-center text-hero Color-gray pt-5 pb-1">
+              <strong style="font-weight: 600;">2500+</strong> Browse Jobs
+            </p>
+            <p class="text-center paragraph-large Color-black pb-5">
+              Find Jobs, Employment & Career Opportunities
+            </p>
+            <div class="row justify-content-md-center">
+              <div class="col-md-10">
+                <form class="form">
+                  <div class="row justify-content-md-center pb-5">
+                    <div class="col-md-5">
+                      <select class="select feature-paragraph Color-gray-60 mb-3">
+                        <option>What are you looking for?</option>
+                      </select>
+                      <select class="select feature-paragraph Color-gray-60">
+                        <option>All Types</option>
+                      </select>
+                    </div>
+                    <div class="col-md-5">
+                      <select class="select feature-paragraph Color-gray-60 mb-3">
+                        <option>Work Schedule</option>
+                      </select>
+                      <select class="select feature-paragraph Color-gray-60">
+                        <option>Proffession</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="row pb-5 justify-content-md-center">
+                    <div class="col-md-2">
+                      <div class="button button-primary button-semi-pill py-1">
+                        Search
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bgColor-gray-10 py-5">
+      <div class="container">
+        <div class="row justify-content-md-center">
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
