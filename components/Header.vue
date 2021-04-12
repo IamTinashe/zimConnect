49e3a62ff0a782fe07d2ea6dc3f9c3c4a28e6e17
IamTinashe <tinashetp@gmail.com>
2021-04-12 19:48:24 +0200

    minor_fix

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="conatiner-fluid">
-    <div class="container-fluid bgColor-black Color-white py-4">
+    <div class="container-fluid bgColor-black Color-white px-4 py-4">
       <div class="row justify-content-md-center mx-lg-2">
         <div class="col-md-5">
           <span>PHONE: +75 8746 958</span>
@@ -8,7 +8,7 @@
         </div>
         <div class="col-md-4 col-lg-5"></div>
         <div class="col-md-3 col-lg-2 text-right">
-          <span class="mr-4">SIGNUP</span>
+          <span class="mr-5">SIGNUP</span>
           <span>SIGNIN</span>
         </div>
       </div>
