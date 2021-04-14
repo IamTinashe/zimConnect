ee1d7a85b15f4cc1c6444e091d28fb3da837087a
IamTinashe <tinashetp@gmail.com>
2021-04-14 16:57:13 +0200

    adds_hire

---
+++ pages/hire.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-100 hire">
+    <div class="container-fluid bgColor-gray-30 py-5">
+      <div class="container">
+        <div class="row hero">
+          <div class="col-md-12 middle-align">
+            <p class="text-center text-hero Color-gray pb-1">
+              HIRE A
+              <br>
+              <strong>TEAM MEMBER</strong>
+            </p>
+            <p class="text-center paragraph-large Color-black pb-5">
+              Find a Team Member Tailored to your specification
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid bgColor-gray-10 py-5">
+      <div class="container bgColor-white py-5">
+        <div class="row justify-content-md-center">
+          Hello World
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
