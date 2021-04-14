9fcb7f384f21234bfba2161cc2966bf9ce95b53d
IamTinashe <tinashetp@gmail.com>
2021-04-14 16:36:17 +0200

    fixes_login_page

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-100">
+  <div class="w-100 login">
     <div class="container-fluid bgColor-gray-10 py-5">
       <div class="container">
-        <div class="row justify-content-md-center">
-          <div class="col-md-12 col-lg-8">
+        <div class="row hero justify-content-md-center">
+          <div class="col-md-12 col-lg-8 middle-align">
             <h2 class="text-center Color-gray-80 subhead py-4">LOGIN</h2>
-            <form class="form pt-3">
+            <form class="form pt-3 pb-5">
               <label class="ml-2 Color-gray-80 feature-paragraph" for="email">Email Address*</label>
               <input type="email" id="email" class="form-input w-100 px-3 py-3 mb-4"/>
 
