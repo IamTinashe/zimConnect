c272f212b38bb6f88d90291ec1ad421b82bba57e
IamTinashe <tinashetp@gmail.com>
2021-04-23 19:05:24 +0200

    corrects_text

--- components/TrendingBox.vue
+++ components/TrendingBox.vue
@@ -5,31 +5,38 @@
         <div class="box-shadow-2 bgColor-white tr-section">
           <div class="row">
             <div class="col-lg-2 bgColor-primary Color-white py-5">
-              <h2 class="text-center title">TRENDING JOBS</h2>
+              <h2 class="text-center py-5 title">TRENDING JOBS</h2>
             </div>
             <div class="col-lg-3 wrapper px-4 py-4">
-              <h3 class="Color-primary feature-paragraph pb-1">Dental Coordinator</h3>
-              <p class="small Color-gray text-justify">
+              <h3 class="Color-primary feature-paragraph pb-1">
+                Dental Coordinator
+              </h3>
+              <br>
+              <p class="small Color-gray">
                 Looking for dynamic people who would to be virtual dental coordinators. All you need
                 is a Bachelor's degree and we will train you and then help place you with the right company.
               </p>
             </div>
-            <div class="col-lg-3 wrapper px-4 py-4">
+            <div class="col-lg-3 wrapper px-3 py-4">
               <h3 class="Color-primary feature-paragraph pb-1">
                 IT Client Liaison/Compliance & Security Officer
               </h3>
-              <p class="small Color-gray text-justify">
-                Looking for dynamic people who would to be virtual dental coordinators. All you need
-                is a Bachelor's degree and we will train you and then help place you with the right company.
+              <p class="small Color-gray">
+                Monitoring network usage to ensure compliance with security policies. Keeping up to date with
+                developments in IT security standards and threats. Performing penetration tests to find any
+                flaws. Collaborating with management and the IT department to improve security. They will be
+                responsible for HIPAA compliance and continually update policies as and when improvements are
+                made to the network.
               </p>
             </div>
             <div class="col-lg-3 px-4 py-4">
               <h3 class="Color-primary feature-paragraph pb-1">
                 Telemarketer
               </h3>
-              <p class="small Color-gray text-justify">
-                Looking for dynamic people who would to be virtual dental coordinators. All you need
-                is a Bachelor's degree and we will train you and then help place you with the right company.
+              <br>
+              <p class="small Color-gray">
+                Need highly aggressive sales people who are great on the phone. Team player who will be
+                interactive with the sales team. Sales experience required.
               </p>
             </div>
           </div>
