efe68e8457992fa9c911865ee120c6232e5e5cbd
IamTinashe <tinashetp@gmail.com>
2021-04-23 16:58:18 +0200

    adds_border

--- components/TrendingBox.vue
+++ components/TrendingBox.vue
@@ -2,19 +2,19 @@
   <div class="bgColor-gray-20 py-5">
     <div class="trending container-fluid">
       <div class="container">
-        <div class="box-shadow-2 border-radius-4 bgColor-white tr-section">
+        <div class="box-shadow-2 bgColor-white tr-section">
           <div class="row">
             <div class="col-lg-2 bgColor-primary Color-white py-5">
               <h2 class="text-center title">TRENDING JOBS</h2>
             </div>
-            <div class="col-lg-3 px-4 py-4">
+            <div class="col-lg-3 wrapper px-4 py-4">
               <h3 class="Color-primary feature-paragraph pb-1">Dental Coordinator</h3>
               <p class="small Color-gray text-justify">
                 Looking for dynamic people who would to be virtual dental coordinators. All you need
                 is a Bachelor's degree and we will train you and then help place you with the right company.
               </p>
             </div>
-            <div class="col-lg-3 px-4 py-4">
+            <div class="col-lg-3 wrapper px-4 py-4">
               <h3 class="Color-primary feature-paragraph pb-1">
                 IT Client Liaison/Compliance & Security Officer
               </h3>
