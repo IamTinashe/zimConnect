86f35a2df31ec0ee6429e9694356477741dd9f29
Tinashe Zvihwati <tinashetp@gmail.com>
2022-03-03 15:39:40 +0200

    corrects_url

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,8 @@ const session = require('express-session');
 require('dotenv').config();
 const manifest = require('./manifest.json');
 //const BASE_URL = 'http://localhost:4000';
-const BASE_URL = 'http://159.69.120.82';
-//const BASE_URL = 'https://worxconnect.org';
-//https://xd.adobe.com/view/fb087c35-f9bc-4a98-9a8f-61b1c78aee12-ae5b/?fullscreenfd
+//const BASE_URL = 'http://159.69.120.82';
+const BASE_URL = 'https://worxconnect.org';
 module.exports = {
   env: {
     Base: BASE_URL,
